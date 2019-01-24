b296c4209648df39e828f0000d5408dc7e3d3bc4
shiv-rajawat <bindassmangal@gmail.com>
2019-01-24 21:08:00 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 terraform init
 terraform apply -var-file=param.tfvars -auto-approve
-#terraform destroy -var-file=param.tfvars -auto-approve
+terraform destroy -var-file=param.tfvars -auto-approve
